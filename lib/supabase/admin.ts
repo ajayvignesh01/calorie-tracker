8bf09ca4c0202a8db1f7c90732b6ac0ed1e8be3d
ajayvignesh01 <ajayvignesh01@gmail.com>
2025-11-10 00:35:18 -0500

    feat: generate supabase types

---
+++ lib/supabase/admin.ts
@@ -0,0 +1,5 @@
+import { Database } from '@/lib/supabase/database.types'
+import { createClient } from '@supabase/supabase-js'
+
+export const createAdminClient = () =>
+  createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_SECRET_DEFAULT_KEY!)
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,7 @@
+import { Database } from '@/lib/supabase/database.types'
 import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const createClient = () => createBrowserClient(supabaseUrl!, supabaseKey!)
+export const createClient = () => createBrowserClient<Database>(supabaseUrl!, supabaseKey!)
